b093842ebed3e5cd81fc17c405af0784788f0536
Samuel <ronysamuel19@gmail.com>
2023-04-10 11:38:51 +0700

    implement soring for Characters Collection

--- build/index.js
+++ build/index.js
@@ -2,7 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
+const CharactersCollection_1 = require("./CharactersCollection");
+// Sorting Numbers Collection
 const numbersCollection = new NumbersCollection_1.NumbersCollection([100, 10, 3, -5, 0, -12]);
 const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+// Sorting Characters Collection
+const charactersCollection = new CharactersCollection_1.CharactersCollection('Xaayb');
+const sorterCharacters = new Sorter_1.Sorter(charactersCollection);
+sorterCharacters.sort();
+console.log(charactersCollection.data);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
+// Sorting Numbers Collection
 const numbersCollection = new NumbersCollection([100, 10, 3, -5, 0, -12]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+// Sorting Characters Collection
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorterCharacters = new Sorter(charactersCollection);
+sorterCharacters.sort();
+console.log(charactersCollection.data);
