0e79a3cf885bf5502410cd558e2b339145917946
Samuel <ronysamuel19@gmail.com>
2023-04-11 22:05:59 +0700

    solving using abstract class

--- build/index.js
+++ build/index.js
@@ -1,15 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Sorter_1 = require("./Sorter");
 const NumbersCollection_1 = require("./NumbersCollection");
 const CharactersCollection_1 = require("./CharactersCollection");
+const LinkedList_1 = require("./LinkedList");
 // Sorting Numbers Collection
 const numbersCollection = new NumbersCollection_1.NumbersCollection([100, 10, 3, -5, 0, -12]);
-const sorter = new Sorter_1.Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
 console.log(numbersCollection.data);
 // Sorting Characters Collection
 const charactersCollection = new CharactersCollection_1.CharactersCollection('Xaayb');
-const sorterCharacters = new Sorter_1.Sorter(charactersCollection);
-sorterCharacters.sort();
+// const sorterCharacters = new Sorter(charactersCollection);
+charactersCollection.sort();
 console.log(charactersCollection.data);
+const linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+// const sorterLinkedList = new Sorter(linkedList);
+linkedList.sort();
+linkedList.print();
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // Sorting Numbers Collection
 const numbersCollection = new NumbersCollection([100, 10, 3, -5, 0, -12]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
 console.log(numbersCollection.data);
 
 // Sorting Characters Collection
 const charactersCollection = new CharactersCollection('Xaayb');
-const sorterCharacters = new Sorter(charactersCollection);
-sorterCharacters.sort();
+// const sorterCharacters = new Sorter(charactersCollection);
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+// const sorterLinkedList = new Sorter(linkedList);
+linkedList.sort();
+linkedList.print();
